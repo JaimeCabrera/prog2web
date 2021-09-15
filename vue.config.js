ddca698f685c77d787f34d443a957e98068d809f
Jaime Cabrera <jycabrerap@unl.edu.ec>
2021-09-15 10:05:21 -0500

    delete tooltip

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "/prog2web/" : "/",
+  publicPath: "/prog2web/",
   // publicPath: process.env.NODE_ENV === "production" ? "/prog2web/" : "/",
 };
